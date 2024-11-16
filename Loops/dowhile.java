5fdb975bcf9e87474b37faf08fe42d687e4033a0
Nitin Choudhary <113338398+Nitin7374@users.noreply.github.com>
2024-11-16 10:36:52 +0530

    Add files via upload

---
+++ Loops/dowhile.java
@@ -0,0 +1,10 @@
+public class dowhile {
+    public static void main(String args[]){
+        int i = 1;
+        do{
+            System.out.println( i + " nitin");
+            i++;
+        }
+        while(i<10);
+    }
+}
